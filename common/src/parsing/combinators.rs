5617b1e182acb5240340d0b3ac3d62c48a4cc35f
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-12-07 17:35:07 +0000

    feat: 2025 d02 (#39)

--- common/src/parsing/combinators.rs
+++ common/src/parsing/combinators.rs
@@ -12,12 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ops::RangeInclusive;
 use std::str::FromStr;
-use winnow::ascii::digit1;
-use winnow::stream::AsChar;
-use winnow::token::take_while;
 use winnow::ModalResult;
 use winnow::Parser;
+use winnow::ascii::digit1;
+use winnow::combinator::separated_pair;
+use winnow::stream::AsChar;
+use winnow::token::take_while;
 
 pub fn parse_number<T: FromStr>(input: &mut &str) -> ModalResult<T> {
     digit1.parse_to().parse_next(input)
@@ -27,4 +29,33 @@ pub fn parse_digit<T: FromStr>(input: &mut &str) -> ModalResult<T> {
     take_while(1, AsChar::is_dec_digit)
         .parse_to()
         .parse_next(input)
+}
+
+pub fn parse_range_inclusive<T: FromStr>(input: &mut &str) -> ModalResult<RangeInclusive<T>> {
+    separated_pair(parse_number, "-", parse_number)
+        .map(|(start, end)| RangeInclusive::new(start, end))
+        .parse_next(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn range_inclusive_parser() {
+        assert_eq!(
+            RangeInclusive::new(11usize, 22),
+            parse_range_inclusive::<usize>.parse("11-22").unwrap()
+        );
+        assert_eq!(
+            RangeInclusive::new(1188511880usize, 1188511890),
+            parse_range_inclusive::<usize>
+                .parse("1188511880-1188511890")
+                .unwrap()
+        );
+        assert_eq!(
+            RangeInclusive::new(998usize, 1012),
+            parse_range_inclusive::<usize>.parse("998-1012").unwrap()
+        );
+    }
 }
